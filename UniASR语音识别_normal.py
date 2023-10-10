a43ecb35dbac3726fbd17083f7b84ba03d589543
D.T <dingtao.egogo@gmail.com>
2023-10-10 22:47:10 +0800

    多角色识别,效果并不明显

--- UniASR语音识别_normal.py
+++ UniASR语音识别_normal.py
@@ -12,30 +12,40 @@ def Paraformer_longaudio_model(use_vad_model=True,use_punc_model=True,use_lm_mod
         vad_model=''
         
     if use_punc_model:
-        punc_model='damo/punc_ct-transformer_zh-cn-common-vocab272727-pytorch'
+        # punc_model='damo/punc_ct-transformer_zh-cn-common-vocab272727-pytorch'
+        punc_model='damo/punc_ct-transformer_cn-en-common-vocab471067-large',
+        
     else:
         punc_model=''
         
+    output_dir = "./results"
     if use_lm_model:
         inference_pipeline = pipeline(
             task=Tasks.auto_speech_recognition,
             # defaults to combine VAD, ASR and PUNC
-            model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+            # model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+            model='damo/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn', # 分角色语音识别
             vad_model=vad_model,
             punc_model=punc_model,
             lm_model='damo/speech_transformer_lm_zh-cn-common-vocab8404-pytorch',
             lm_weight=0.15,
             beam_size=10,
-            model_revision=None,
+            # model_revision=None,
+            model_revision='v0.0.2',
+            output_dir=output_dir,
             )
+        
     else:
         inference_pipeline = pipeline(
             task=Tasks.auto_speech_recognition,
             # defaults to combine VAD, ASR and PUNC
-            model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+            # model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+            model='damo/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn', # 分角色语音识别
             vad_model=vad_model,
             punc_model=punc_model,
-            model_revision=None,
+            # model_revision=None,
+            model_revision='v0.0.2',
+            # output_dir=output_dir,
             )           
         
     return inference_pipeline #这里先输出模型, 以避免后续模型重复生成;
@@ -50,14 +60,20 @@ def Paraformer_longaudio_recognition(inference_pipeline, audio_data,use_timestam
     rec_result = inference_pipeline(audio_in=waveform)#, param_dict=param_dict)
     return rec_result
 
-def audio_source(source):
+def audio_source(source,url):
     if source == 'upload':
+        inp_url = gr.Textbox( visible=False)
         out = gr.Audio(source="upload",)
     elif source == 'microphone':
+        inp_url = gr.Textbox( visible=False)
         out = gr.Audio(source='microphone',)
+    elif source == 'url':
+        inp_url = gr.Textbox( visible=True)
+        out = gr.Audio(value=url,source=None)
+        # inp_url.input(lambda x: x, inp_url,out)
     else:
         print('invalid source')
-    return out
+    return out, inp_url
 
 def model_checkbox(model_selected):
     use_vad_model = True if "VAD" in model_selected else False
@@ -74,7 +90,7 @@ def run(audio_data,model_selected, use_timestamp=True):
     """
     sample_rate, waveform = audio_data
     waveform = waveform.astype(np.float32) # gr.Audio转换的为(sample rate in Hz, audio data as a 16-bit int array);而输入pipeline需要float32, 双声道转换成单声道;
-    if len(waveform.shape)>=1: # 说明是双声道
+    if len(waveform.shape)>1: # 说明是双声道
         waveform = np.mean(waveform,axis=1) # 转换成单声道
     # waveform, sample_rate = soundfile.read(audio_data)
     inference_pipeline = model_checkbox(model_selected)
@@ -105,19 +121,21 @@ if __name__ == "__main__":
         with gr.Row():
             with gr.Column(variant='panel'):
                 inp0 = gr.Radio(
-                    choices=["microphone", "upload"],
+                    choices=["microphone", "upload", "url"],
                     value="upload",
                     type="value",
                     label="选择音频来源",
                     show_label=True,
                     )
+                inp_url = gr.Textbox(lines=1,placeholder='https://', label='输入音频链接',show_label=True,show_copy_button=True,
+                                    value='https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_speaker_demo.wav')
                 inp1 = gr.Audio(
-                    source="upload",
+                    # source=None,
                     type="numpy",
                     show_label=True,
                     interactive=True,
                     )
-                inp0.change(audio_source,inp0, inp1)
+                inp0.change(audio_source,[inp0, inp_url],  [inp1, inp_url])
                 with gr.Row(variant='panel'):
                     inp2 = gr.CheckboxGroup(["VAD", "PUNC","NNLM"], label="请选择是否在ASR模型外,包含下面模型",
                                         value=["VAD", "PUNC"],show_label=True )
@@ -127,11 +145,15 @@ if __name__ == "__main__":
                      
                     inp3 = gr.Checkbox(value=True,label='时间戳', show_label=True)                
             with gr.Column(variant='panel'):
-                out0 = gr.Textbox(lines=6,placeholder='语音识别为:', label='识别结果:',show_label=True,show_copy_button=True)
+                out0 = gr.Textbox(lines=6,placeholder='语音识别为:', label='识别文本......',show_label=True,show_copy_button=True)
             
-        button = gr.Button(value='一键识别', variant='primary')
-        button.click(run, [inp1,inp2,inp3], out0)
-        # button.click(debug,model_state, out0)
+        with gr.Row(variant='panel'):
+            submit = gr.Button(value='一键识别', variant='primary')
+            submit.click(run, [inp1,inp2,inp3], out0)
+            clear = gr.Button(value='清除',variant='primary')
+            
+            clear.click(lambda : "" , outputs=out0)
+        
         
             
         demo.queue()
