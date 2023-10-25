af946267e6be69a5b5366da21a4fad63bfbd0d72
D.T <dancingonmoon@msn.com>
2023-10-25 16:26:36 +0800

    stream_mode

--- ParaformerOnline.py
+++ ParaformerOnline.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # import os
 import logging
+
 # import soundfile
 import librosa
 
@@ -14,18 +15,18 @@ logger = get_logger(log_level=logging.CRITICAL)
 logger.setLevel(logging.CRITICAL)
 
 # os.environ["MODELSCOPE_CACHE"] = "./"
-output_dir = './result'
+output_dir = "./result"
 inference_pipeline_vad = pipeline(
     task=Tasks.voice_activity_detection,
     model="damo/speech_fsmn_vad_zh-cn-16k-common-pytorch",
     model_revision=None,
     output_dir=output_dir,
     batch_size=1,
-    mode='online',
+    mode="online",
 )
 inference_pipeline = pipeline(
     task=Tasks.auto_speech_recognition,
-    model='damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online',
+    model="damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online",
     model_revision=None,
     # update_model=False,
     mode="paraformer_streaming",
@@ -33,7 +34,7 @@ inference_pipeline = pipeline(
 )
 inference_pipeline_punc = pipeline(
     task=Tasks.punctuation,
-    model='damo/punc_ct-transformer_zh-cn-common-vad_realtime-vocab272727',
+    model="damo/punc_ct-transformer_zh-cn-common-vad_realtime-vocab272727",
     model_revision=None,
 )
 
@@ -41,9 +42,10 @@ inference_pipeline_punc = pipeline(
 # model_dir = os.path.join(os.environ["MODELSCOPE_CACHE"], "damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online")
 # audio_data, sample_rate = soundfile.read(os.path.join(model_dir, "example/asr_example.wav"))
 
+
 def RUN(audio_stream, speech_txt):
     """
-    speech_txt: 之前累计的识别文本
+    speech_txt: 之前累计的识别文本punc后的列表
     """
     samplerate = 16000  # 初始赋值,避免警告;
     speech = np.empty(shape=None, dtype=np.float32)
@@ -63,77 +65,91 @@ def RUN(audio_stream, speech_txt):
 
     if isinstance(audio_stream, str):
         # librosa.load 就可以解决采样率,单声道,float32
-        speech, samplerate = librosa.load(audio_stream, sr=16000, mono=True, offset=0.0, duration=None,
-                                          dtype=np.float32)
+        speech, samplerate = librosa.load(
+            audio_stream,
+            sr=16000,
+            mono=True,
+            offset=0.0,
+            duration=None,
+            dtype=np.float32,
+        )
 
     speech_length = speech.shape[0]
-
-    # speech_stride_vad_segments = inference_pipeline_vad(audio_in=speech)
 
     sample_offset = 0
     chunk_size = [5, 10, 5]  # 第一个5为左看5帧,10为text_n 10帧,为600ms, 第二个5为右看5帧
     # stride_size = chunk_size[1] * 960 # 为什么是960 ?
     stride_size = chunk_size[1] * 960
     param_dict = {"cache": dict(), "is_final": False, "chunk_size": chunk_size}
-    final_result = ""
+    param_punc = {"cache": []}
+    # final_result = ""
+    punc_list = []  # list以送入punc online mode
+    # punc_list.append(speech_txt) # 将之前的punc后的txt,作为vad之后的第一个,再送入pineline_punc()
 
-    for sample_offset in range(0, speech_length, min(stride_size, speech_length - sample_offset)):
+    for sample_offset in range(
+        0, speech_length, min(stride_size, speech_length - sample_offset)
+    ):
         if sample_offset + stride_size >= speech_length - 1:
             stride_size = speech_length - sample_offset
             param_dict["is_final"] = True
         else:
             param_dict["is_final"] = False
 
-        speech_stride = speech[sample_offset:sample_offset + stride_size]
+        speech_stride = speech[sample_offset : sample_offset + stride_size]
         # 1. VAD:
-        speech_stride_vad_segments = inference_pipeline_vad(audio_in=speech_stride,
-                                                            param_dict=param_dict)
+        speech_stride_vad_segments = inference_pipeline_vad(
+            audio_in=speech_stride, param_dict=param_dict
+        )
         print(f"vad_segments:{speech_stride_vad_segments}")
         # 2. ASR:
-        if 'text' in speech_stride_vad_segments:  # 检测到音频数据后，每隔600ms进行一次流式模型推理
-            for i, segments in enumerate(speech_stride_vad_segments['text']):
+        if "text" in speech_stride_vad_segments:  # 检测到音频数据后，每隔600ms进行一次流式模型推理
+            for i, segments in enumerate(speech_stride_vad_segments["text"]):
                 beg_idx = segments[0] * samplerate / 1000
                 end_idx = segments[1] * samplerate / 1000
-                speech_stride_vad_clip = speech_stride[int(beg_idx):int(end_idx)]
+                speech_stride_vad_clip = speech_stride[int(beg_idx) : int(end_idx)]
                 # speech_stride_vad_clip = speech[int(beg_idx):int(end_idx)] # 取speech会导致错误
-                speech_stride_vad_clip_result = inference_pipeline(audio_in=speech_stride_vad_clip,
-                                                                   param_dict=param_dict)
-                print(f"vad_{i}: {beg_idx}->{end_idx}: stride_vad_clip_result:{speech_stride_vad_clip_result}")
+                speech_stride_vad_clip_result = inference_pipeline(
+                    audio_in=speech_stride_vad_clip, param_dict=param_dict
+                )
+                print(
+                    f"vad_{i}: {beg_idx}->{end_idx}: stride_vad_clip_result:{speech_stride_vad_clip_result}"
+                )
 
                 # 3. punc
                 # if speech_stride_vad_clip_result != []: # 例子中的代码,应该是错误的
                 # (https://github.com/alibaba-damo-academy/FunASR/discussions/278)
-                if len(speech_stride_vad_clip_result) != 0:  # 语音识别出非空时,
-                    param_dict['cache'] = []
+                # the online model is used at each time of speech absence of voice activity detection(VAD)
+                # and should be feed the cache extracted from the history text.
+                if "text" in speech_stride_vad_clip_result:  # 语音识别出非空时,
                     speech_stride_vad_clip_punc_result = inference_pipeline_punc(
                         text_in=speech_stride_vad_clip_result['text'],
-                        param_dict=param_dict)
+                        param_dict=param_punc)
                     print(
-                        f"punc_{i}: {beg_idx}->{end_idx}: stride_vad_clip_punc_result:{speech_stride_vad_clip_punc_result}")
-                #
+                    f"punc_{i}: {beg_idx}->{end_idx}: stride_vad_clip_result:{speech_stride_vad_clip_punc_result}")
+                    # print(f"speech_txt: {speech_txt}")
                 else:
                     speech_stride_vad_clip_punc_result = speech_stride_vad_clip_result
-                # 4. 拼接
-                if len(speech_stride_vad_clip_punc_result) != 0:
-                    final_result += speech_stride_vad_clip_punc_result['text']
 
-                param_dict['cache'] = dict()
-        # else: # 当在说话停顿处，做标点断句恢复，修正识别文字:
+                if 'text' in speech_stride_vad_clip_punc_result:
+                    speech_txt += speech_stride_vad_clip_punc_result["text"]
 
-        # if len(rec_result) != 0:
-        #     final_result += rec_result['text']
-        #     print(rec_result)
-
-    speech_txt += final_result
     return speech_txt, speech_txt
 
 
-with gr.Blocks(theme='soft', title='实时识别') as demo:
-    with gr.Row(variant='panel'):
-        audio_stream = gr.Audio(source='microphone', type='filepath', label='请录音并实时说话', streaming=True, )
+with gr.Blocks(theme="soft", title="实时识别") as demo:
+    with gr.Row(variant="panel"):
+        audio_stream = gr.Audio(
+            source="microphone",
+            type="filepath",
+            label="请录音并实时说话",
+            streaming=True,
+        )
         speech_txt = gr.State("")
-        out = gr.Textbox(lines=2, placeholder='实时识别....', show_copy_button=True)
+        out = gr.Textbox(lines=2, placeholder="实时识别....", show_copy_button=True)
 
         audio_stream.stream(RUN, [audio_stream, speech_txt], [out, speech_txt])
 
-demo.launch(debug=True, show_error=True, )
+demo.launch(
+    debug=True,
+    show_error=True,
+)
