538fd4370f9ec3800a8845123ef2c398913d8e6b
D.T <dancingonmoon@msn.com>
2024-02-22 17:15:37 +0800

    SerpAPI+zhipuaiSDK

--- GLM/GLM_SDK练习.py
+++ GLM/GLM_SDK练习.py
@@ -114,34 +114,35 @@ query = "风能对环境有什么好处？"
 # print(samples)
 
 # retrieval:
-knowledge = "百度引擎"
+knowledge_id = 1759942607489871872 # 百度搜索
 question = "Tucker Carson与普京的会面,都谈了些什么?"
+
+prompt_template = """
+从文档
+{{knowledge}}
+中找问题
+{{question}}
+的答案，
+找到答案就仅使用文档语句回答，找不到答案就用自身知识回答并告诉用户该信息不是来自文档。
+不要复述问题，直接开始回答。
+"""
 
 response = client.chat.completions.create(
     model="glm-4",  # 填写需要调用的模型名称
     messages=[
-        {"role": "user", "content": "你好！你叫什么名字"},
+        {"role": "user", "content": question},
     ],
     tools=[
             {
                 "type": "retrieval",
                 "retrieval": {
                     "knowledge_id": "your knowledge id",
-                    "prompt_template":"从文档\n
-                    """\n
-                    {{knowledge}}\n
-                    """\n
-                    中找问题\n
-                    """\n
-                    {{question}}\n
-                    """\n
-                    的答案，找到答案就仅使用文档语句回答，找不到答案就用自身知识回答并告诉用户该信息不是来自文档。\n
-
-                    不要复述问题，直接开始回答。"
+                    "prompt_template": prompt_template,
                 }
             }
             ],
-    stream=True,
+    stream=False,
 )
-for chunk in response:
-    print(chunk.choices[0].delta)
+# for chunk in response:
+#     print(chunk.choices[0].delta)
+print(response.choices[0].message.content)
---
+++ GLM/SerpAPI_fn.py
@@ -0,0 +1,74 @@
+# https://serpapi.com/search-api
+
+from serpapi import GoogleSearch
+import configparser
+
+
+def get_api_key(config_file_path, section='Serp_API', option='api_key'):
+    """
+    从配置文件config.ini中,读取api_key,api_secret;避免程序代码中明文显示key,secret.
+    args:
+        config_file_path: config.ini的文件路径(包含文件名,即: directory/config.ini)
+        section: config.ini中的section名称;
+        option: config.ini中的option名称;
+    out:
+        返回option对应的value值;此处为api_key
+    """
+    config = configparser.ConfigParser()
+    config.read(config_file_path, encoding="utf-8")  # utf-8支持中文
+    return config[section][option]
+
+
+def serpapi_search(config_path, query, section='Serp_API', option='api_key', , location='Hong Kong', hl='zh-cn',
+                   gl='cn',
+                   tbs=None, tbm=None,
+                   num=30, ):
+    """
+    使用SerpAPI进行Google搜索
+    args:
+        config_path: config.ini的文件路径(包含文件名,即: directory/config.ini)
+        section: config.ini中section名称;
+        option: config.ini中option名称;
+        query: 搜索的问题或关键字
+        location: Parameter defines from where you want the search to originate.
+        hl:Parameter defines the country to use for the Google search. It's a two-letter country code. (e.g., us for the United States, uk for United Kingdom, or fr for France)
+        gl:Parameter defines the language to use for the Google search. It's a two-letter language code. (e.g., en for English, es for Spanish, or fr for French). Head to the Google languages page for a full list of supported Google languages.
+        num:Parameter defines the maximum number of results to return. (e.g., 10 (default) returns 10 results
+        tbs:(to be searched) parameter defines advanced search parameters that aren't possible in the regular query field. (e.g., advanced search for patents, dates, news, videos, images, apps, or text contents).
+        tbm:(to be matched) parameter defines the type of search you want to do.
+            It can be set to:
+            (no tbm parameter): regular Google Search,
+            isch: Google Images API,
+            lcl - Google Local API
+            vid: Google Videos API,
+            nws: Google News API,
+            shop: Google Shopping API,
+            pts: Google Patents API,
+            or any other Google service.
+
+    out:
+        result: a structured JSON of the google search results
+    """
+    api_key = get_api_key(config_path, section, option)
+    param = {
+        "q": query,
+        "location": location,
+        "api_key": api_key,
+        "hl": hl,
+        "gl": gl,
+        "num": num,
+        "tbm": tbm,
+        "tbs": tbs
+    }
+    search = GoogleSearch(param)
+    result = search.get_dict()
+    return result
+
+
+if __name__ == "__main__":
+    config_path = r"E:/Python_WorkSpace/config/SerpAPI.ini"
+    query = 'Tucker Carson与普京的会面,都谈了些什么?'
+    # api_key = get_api_key(config_path, 'Serp_API', 'api_key')
+    # print(api_key)
+    search_result = serpapi_search(config_path, 'Serp_API', 'api_key', query, 'Hong Kong', 'zh-cn', 'cn', )
+    print(search_result)
