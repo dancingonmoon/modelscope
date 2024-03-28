36e9eb9c8917056f87f3dc3e5179895a40a4fe30
D.T <dingtao.egogo@gmail.com>
2024-03-28 22:55:53 +0800

    aliyun TTS api

--- DingTalkAPP/TTS_SSML.py
+++ DingTalkAPP/TTS_SSML.py
@@ -10,24 +10,24 @@ import nls
 from ChatBOT_APP import config_read, setup_logger
 
 
-# def emo_label_out(data):
-#     scores = data['scores']
-#     max_value = max(scores)
-#     max_index = scores.index(max_value)
-#     label = data['labels'][max_index]
-#     return label
+def emo_label_out(data):
+    scores = data['scores']
+    max_value = max(scores)
+    max_index = scores.index(max_value)
+    label = data['labels'][max_index]
+    return label
 
 
 # semantic_cls = pipeline(Tasks.text_classification, 'damo/nlp_structbert_emotion-classification_chinese-base', model_revision='v1.0.0')
 
-def emo_classification(text):
-    """
-    中文情绪分类: labels': ['恐惧', '高兴', '悲伤', '喜好', '厌恶', '愤怒', '惊讶']
-    """
-    emo_dict = semantic_cls(input=text)  # 输出dict
-    label = emo_label_out(emo_dict)
-    # print(label)
-    return label
+# def emo_classification(text):
+#     """
+#     中文情绪分类: labels': ['恐惧', '高兴', '悲伤', '喜好', '厌恶', '愤怒', '惊讶']
+#     """
+#     emo_dict = semantic_cls(input=text)  # 输出dict
+#     label = emo_label_out(emo_dict)
+#     # print(label)
+#     return label
 
 
 # SSML（Speech Synthesis Markup Language）:pip install aliyun-python-sdk-core
@@ -45,7 +45,7 @@ def emo_classification(text):
 
 
 # 获取 accessKey_id, accessKey_secret:
-config_path_aliyunsdk = r"e:/Python_WorkSpace/config/aliyunsdkcore.ini"
+config_path_aliyunsdk = r"l:/Python_WorkSpace/config/aliyunsdkcore.ini"
 accessKey_id, accessKey_secret = config_read(config_path_aliyunsdk, section='aliyunsdkcore', option1='AccessKey_ID',
                                              option2='AccessKey_Secret')
 
@@ -83,7 +83,7 @@ def get_token_viaSDK(accessKey_id, accessKey_secret, region_id='cn-shanghai'):
         return e
 
 
-token = get_token_viaSDK(accessKey_id, accessKey_secret)
+token, expirTime = get_token_viaSDK(accessKey_id, accessKey_secret)
 appKey = config_read(config_path_aliyunsdk, section='APP_tts', option1='AppKey')
 # URL = "wss://nls-gateway-cn-shanghai.aliyuncs.com/ws/v1"
 URL = "wss://nls-gateway.aliyuncs.com/ws/v1"  # 就近地域智能接入,自动根据地域选择shanghai/beijing/shenzhen
@@ -97,9 +97,10 @@ def fun_on_metainfo(message, *args):
     1)JSON形式的字符串 2)用户自定义参数
     其中，用户自定义参数为callback_args字段中返回的参数内容。
     """
-    return f"appKey:{args}; subtitle:{message}"
+    print(f"appKey:{args}; subtitle:{message}")
+    # return f"appKey:{args}; subtitle:{message}"
 
-
+f = open(tts_out_path, 'wb')
 def fun_on_data(data, *args):
     """
     当存在合成数据后的回调参数。回调参数包含以下两种：
@@ -107,21 +108,43 @@ def fun_on_data(data, *args):
     """
     tts_out_path = args[1]
     try:
-        with open(tts_out_path, 'wb') as f:
-            f.write(data)
+        # with open(tts_out_path, mode='wb') as f:
+        f.write(data)
+        print(f"write data finish")
+        # print(f"received:{data}")
     except Exception as e:
         print("write data failed:", e)
 
+def fun_on_error(message, *args):
+    """
+    当SDK或云端出现错误时的回调参数。回调参数包含以下两种：
+    1)JSON形式的字符串 2)用户自定义参数
+   其中，用户自定义参数为callback_args字段中返回的参数内容。
+    """
+    print(f"error:{message}")
 
+def fun_on_close(*args):
+    print("on_close: args=>{}".format(args))
+    try:
+        f.close()
+    except Exception as e:
+        print("close file failed since:", e)
+
+nls.enableTrace(True)
 tts = nls.NlsSpeechSynthesizer(url=URL,
                                token=token,
                                appkey=appKey,
                                on_metainfo=fun_on_metainfo,
                                on_data=fun_on_data,
                                # on_completed=fun_on_completed,
-                               # on_error=fun_on_error,
-                               # on_close=fun_on_close,
+                               on_error=fun_on_error,
+                               on_close=fun_on_close,
                                callback_args=[appKey, tts_out_path])
-tts.start(text=TEXT, aformat='wav', voice='zhitian_emo',
-          speech_rate=0, pitch_rate=0, wait_complete=True,
-          ex={"enable_subtitle": True, "enable_ptts": False})
+result = tts.start(text=TEXT, aformat='wav', voice='zhitian_emo',
+                   speech_rate=0, pitch_rate=0, wait_complete=True,
+                   ex={"enable_subtitle": True, "enable_ptts": False})
+
+
+print(result)
+
+
