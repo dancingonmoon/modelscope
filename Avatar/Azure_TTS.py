e76a8ebc8c00e64359bfb12582f36c62a090590e
D.T <dancingonmoon@msn.com>
2024-04-28 17:11:12 +0800

    azure TTS class realization

--- Avatar/Azure_TTS.py
+++ Avatar/Azure_TTS.py
@@ -2,12 +2,15 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 import configparser
 
-def config_read(config_path, section='DingTalkAPP_chatGLM', option1='Client_ID', option2=None):
+
+def config_read(
+    config_path, section="DingTalkAPP_chatGLM", option1="Client_ID", option2=None
+):
     """
     option2 = None 时,仅输出第一个option1的值; 否则输出section下的option1与option2两个值
     """
     config = configparser.ConfigParser()
-    config.read(config_path, encoding='utf-8')
+    config.read(config_path, encoding="utf-8")
     option1_value = config.get(section=section, option=option1)
     if option2 is not None:
         option2_value = config.get(section=section, option=option2)
@@ -16,16 +19,49 @@ def config_read(config_path, section='DingTalkAPP_chatGLM', option1='Client_ID',
         return option1_value
 
 
+def azure_TTS(key, region, text):
+    """
+    使用azure TTS 实现TTS,输出格式内存对象(流形式),或者缺省扬声器,音频格式为Ogg16Khz16BitMonoOpus
+    :return:
+    """
+    speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
+    speech_config.set_speech_synthesis_output_format(
+        speechsdk.SpeechSynthesisOutputFormat.Ogg16Khz16BitMonoOpus
+    )
+    speech_config.speech_synthesis_voice_name = (
+        "zh-CN-XiaoxiaoNeural"  # "en-US-AvaMultilingualNeural"
+    )
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, audio_config=None
+    )
+    result = speech_synthesizer.speak_text_async(text).get()
+    stream = speechsdk.AudioDataStream(result)
+    # stream.save_to_wav_file("path/to/write/file.wav")
+
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        # print("Speech synthesized for text [{}]".format(text))
+        return stream
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        reason =  "Speech synthesis canceled: {}".format(cancellation_details.reason)
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            reason = "Error details: {}".format(cancellation_details.error_details)
+
+
 
 
 config_path = r"e:\Python_WorkSpace\config\Azure_Resources.ini"
 # Creates an instance of a speech config with specified subscription key and service region.
-key, region = config_read(config_path=config_path, section='Azure_TTS',option1='key', option2='region')
+key, region = config_read(
+    config_path=config_path, section="Azure_TTS", option1="key", option2="region"
+)
 
 speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 # Note: the voice setting will not overwrite the voice element in input SSML.
-speech_config.speech_synthesis_voice_name = "zh-CN-XiaoxiaoNeural" #"en-US-AvaMultilingualNeural"
+speech_config.speech_synthesis_voice_name = (
+    "zh-CN-XiaoxiaoNeural"  # "en-US-AvaMultilingualNeural"
+)
 
 text = "what a wonderful day神奇的一天"
 ssml_string = """
@@ -50,7 +86,9 @@ ssml_string = """
 """
 
 # use the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+speech_synthesizer = speechsdk.SpeechSynthesizer(
+    speech_config=speech_config, audio_config=audio_config
+)
 
 # result = speech_synthesizer.speak_text_async(text).get()
 result = speech_synthesizer.speak_ssml_async(ssml_string).get()
@@ -62,7 +100,3 @@ elif result.reason == speechsdk.ResultReason.Canceled:
     print("Speech synthesis canceled: {}".format(cancellation_details.reason))
     if cancellation_details.reason == speechsdk.CancellationReason.Error:
         print("Error details: {}".format(cancellation_details.error_details))
-
-
-
-
