ef2d8672e2b673b1b34683f40f183b1d256dc75c
D.T <dancingonmoon@msn.com>
2024-05-06 17:06:42 +0800

    flask + wechat

--- WeChat/Flask练习.py
+++ WeChat/Flask练习.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
-
+from markupsafe import escape
 
 app = Flask(__name__)
 @app.route("/")
+def index():
+    return 'Index Page'
+@app.route("/hello")
 def hello():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello , World!</p>"
+@app.route("/<name>")
+def hello_world(name):
+    return f"<p>Hello {escape(name)} , World!</p>"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=8080, debug=True)
