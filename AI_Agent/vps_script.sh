72ffb678562dd547a23b1d7074f4ad674a5b46f5
D.T <dancingonmoon@msn.com>
2024-11-30 22:23:45 +0300

    vps—â_script.sh

---
+++ AI_Agent/vps_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 安装anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash Anaconda3-2024.10-1-Linux-x86_64.sh
+# conda --version
+source ~/.bashrc
+conda --version
+# 创建conda环境
+conda create -n myenv python=3.12
+conda activate myenv
+# 升级conda
+conda update -n base -c defaults conda
+
+# pip install -q -U google-generativeai
+
+# 允许某个端口可以被外部访问；
+sudo ufw allow 7860
