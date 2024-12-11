8bd047a11698a6931770e9bfecfda849a7787fd6
D.T <dingtao.egogo@gmail.com>
2024-12-11 21:58:49 +0800

    V2Ray

--- AI_Agent/vps_script.sh
+++ AI_Agent/vps_script.sh
@@ -17,3 +17,16 @@ conda update -n base -c defaults conda
 
 # 允许某个端口可以被外部访问；
 sudo ufw allow 7860
+
+# 以下为安装V2Ray 梯子服务器搭建：
+# VPS服务器需要进行服务器时间同步校对时就需要用到时间同步组件ntp，使服务器时间保持跟全球网络同步
+apt-get install ntp ntpdate -y
+#安装完成后，先停止当前 VPS 服务器的 NTP 服务
+service ntpd stop
+#然后再使当前 VPS 服务器的时间与时间服务器进行同步
+ntpdate us.pool.ntp.org
+#最后启动 NTP 服务
+service ntpd start
+
+#执行V2Ray一键安装脚本
+bash <(wget -qO- -o- https://git.io/v2ray.sh)
