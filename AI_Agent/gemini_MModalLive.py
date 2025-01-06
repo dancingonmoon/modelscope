263fac715b2dd035e6091bc022f182bf088bff85
D.T <dancingonmoon@msn.com>
2025-01-06 14:25:44 +0800

    rewrite gemini 2.0 SDK async multi modal live API

--- AI_Agent/gemini_MModalLive.py
+++ AI_Agent/gemini_MModalLive.py
@@ -1,9 +1,14 @@
 import asyncio
 from google import genai
-import wave
 import logging
 import traceback
 import pyaudio
+import mss
+from PIL import Image
+import io
+import base64
+import webrtcvad
+import numpy as np
 
 CHANNELS = 1
 RECEIVE_SAMPLE_RATE = 16000  # 输入音频格式：16kHz 小端字节序的原始 16 位 PCM 音频
@@ -50,18 +55,6 @@ async def txt2txt():
                 print(response.text, end="")
 
 
-async def async_enumerate(it):
-    """
-    对生成器迭代内容,进行异步的enumerate,即输出迭代内容的序列号,以及迭代内容本身
-    :param it: 一个迭代器
-    :return: 返回迭代的序列号,以及单次迭代内容
-    """
-    n = 0
-    async for item in it:
-        yield n, item
-        n += 1
-
-
 class GeminiLiveStream:
     """
     implements the interaction with the Live API：
@@ -81,7 +74,7 @@ class GeminiLiveStream:
         config=None,
         pyaudio_instance: pyaudio.PyAudio = None,
         logger: logging.Logger = None,
-        model: str = "models/gemini-2.0-flash-exp"
+        model: str = "models/gemini-2.0-flash-exp",
     ):
         """ """
         self.model = model
@@ -115,15 +108,15 @@ class GeminiLiveStream:
         self.logger.debug("connect")
         try:
             async with (
-                client.aio.live.connect(model=self.model, config=self.config) as session,
+                client.aio.live.connect(
+                    model=self.model, config=self.config
+                ) as session,
                 asyncio.TaskGroup() as tg,
             ):
                 self.session = session
                 send_txt_task = tg.create_task(self.send())
                 tg.create_task(self.recv())
-                tg.create_task(
-                    self.play_audio(sample_width, channels, rate)
-                )
+                tg.create_task(self.play_audio(sample_width, channels, rate))
 
                 # 此处等待send_txt_task结束，而实际上self.send()函数是无限循环的input,除非user输入了exit，主动退出；
                 # self.send()函数中的break，会退出self.send()函数，而不会退出async with 语句块;
@@ -223,6 +216,101 @@ class GeminiLiveStream:
                 stream.close()
                 self.logger.info("user_command终止")
                 break
+
+    def _get_screen(self):
+        with mss.mss() as sct:
+            # monitor = sct.monitors[0]
+            # i = sct.grab(monitor)  # 捕获指定显示器的截图
+            screenshot = sct.shot()  # 捕获活动屏幕的截图
+
+        image_bytes = mss.tools.to_png(screenshot.rgb, screenshot.size)  # raw data
+        img = Image.open(io.BytesIO(image_bytes))  # image data bytes
+        # 写入 文件格式的内存对象
+        image_io = io.BytesIO()
+        img.save(image_io, format="jpeg")
+        image_io.seek(0)
+
+        image_bytes = image_io.read()
+        mime_type = "image/jpeg"
+        return {
+                "mime_type": mime_type,
+                "data": base64.b64encode(image_bytes).decode(),
+            }
+
+    async def get_screen(self):
+
+        while True:
+            frame = await asyncio.to_thread(self._get_screen)
+            if frame is None:
+                self.logger.info("failed to get screen")
+                break
+            await asyncio.sleep(1.0)
+            await self.in_queue.put(frame)
+
+    async def send_realtime(self):
+        while True:
+            msg = await self.in_queue.get()
+            await self.session.send(msg)
+
+    async def microphone_audio(
+            self,
+            sample_width: int = 2,
+            channels: int = 1,
+            rate: int = 16000,
+            chunk_size: int = 480,  # 16Khz, 30ms长度,对应的帧长度是480
+            vad_mode: int = 2,  # vad 模式，0-3，3最敏感
+    ):
+        """
+        持续不断的从麦克风读取音频数据;使用asyncio.Queue来缓存队列,传递异步进程的音频数据,音频输入输出更加光滑;
+        实现了回声抑制(经过vad判断is_speech,静音填充) [注: 可能有vad算法的原因,回声抑制效果不是最佳,可以考虑更换回声抑制算法库]
+        """
+        vad = webrtcvad.Vad(vad_mode)
+        mic_info = self.pyaudio_instance.get_default_input_device_info()
+        audio_stream = await asyncio.to_thread(
+            self.pyaudio_instance.open,
+            format=self.pyaudio_instance.get_format_from_width(sample_width),
+            channels=channels,
+            rate=rate,
+            input=True,
+            input_device_index=mic_info["index"],
+            frames_per_buffer=chunk_size,
+        )
+        if __debug__:  # 这是一个条件语句，用于检查当前是否处于调试模式。
+            kwargs = {
+                "exception_on_overflow": False
+            }  # 如果处于调试模式，kwargs 被设置为一个字典，这意味着在调试模式下，当音频流溢出（即缓冲区满，无法再接收更多数据）时，不会抛出异常。
+        else:
+            kwargs = {}  # 如果当前不是调试模式，这意味着在生产模式下，当音频流溢出时，可能会抛出异常，这通常是为了确保程序能够及时处理错误情况
+
+        try:
+            while True:
+                if self.pause_stream:
+                    await asyncio.sleep(.1)
+                    continue
+                elif not self.stop_stream:
+                    try:
+                        data = await asyncio.to_thread(audio_stream.read, chunk_size, **kwargs)
+                        data_np = np.frombuffer(data, dtype=np.int16)
+                        # 使用VAD检测是否是语音
+                        is_speech = vad.is_speech(data_np.tobytes(), rate)
+                        if is_speech:
+                            audio_vad = data
+                        else:
+                            # 填补静音数据
+                            silent_frame = np.zeros(chunk_size, dtype=np.int16)
+                            audio_vad = silent_frame.tobytes()
+                        await self.audio_out_queue.put(audio_vad)
+                    except OSError as e:
+                        self.logger.error(f"麦克风读取发生操作系统错误: {e}")
+                        break  # 发生错误时退出循环
+                elif self.stop_stream:
+                    audio_stream.stop_stream()
+                    audio_stream.close()
+                    self.logger.info('麦克风停止录音')
+                    break
+        except ExceptionGroup as EG:
+            traceback.print_exception(EG)
+            self.logger.info(f"麦克风初始化或读取发生错误: {EG}")
 
 
 if __name__ == "__main__":
