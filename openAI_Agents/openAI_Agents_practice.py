6e1e4a631d81043db784280ce4b7c7afa60c7700
D.T <dancingonmoon@msn.com>
2025-05-27 15:22:32 +0800

    openAI-Agents mcp

--- openAI_Agents/openAI_Agents_practice.py
+++ openAI_Agents/openAI_Agents_practice.py
@@ -2,7 +2,8 @@ import os
 import asyncio
 from openai import AsyncOpenAI
 from openai.types.responses import ResponseTextDeltaEvent
-from agents import OpenAIChatCompletionsModel, Agent, Runner, set_default_openai_client, set_tracing_disabled, WebSearchTool
+from agents import OpenAIChatCompletionsModel, Agent, Runner, set_default_openai_client, set_tracing_disabled, \
+    WebSearchTool
 from agents.model_settings import ModelSettings
 from rich import print
 from rich.markdown import Markdown
@@ -26,7 +27,8 @@ def custom2default_openai_model(model: str, base_url: str, api_key: str, ):
 async def agents_async_chat_once(agent: Agent, input_items: list[dict],
                                  runner_mode: Literal['async', 'stream'] = 'async'):
     """
-    输入[{"role": "user", "content": prompt}]格式prompt,输出agent的result.to_input_list(),即全部历史记录；
+    输入[{"role": "user", "content": prompt}]格式prompt,输出agent的result类，可以通过result.new_items属性来查看全部的事件；
+    result.new_items[0].raw_item，可以看具体的回复内容；to_input_list()方法，可以直接将用户的输入和本次输出结果拼接成一个消息列表
     :param agent:
     :param input_items: list[dict],表示输入的prompt格式列表，例如: [{"role": "user", "content": prompt}]
     :param runner_mode:
@@ -36,26 +38,24 @@ async def agents_async_chat_once(agent: Agent, input_items: list[dict],
     if runner_mode == 'async':
         result = await Runner.run(agent, input_items)
         print(Markdown(result.final_output))
-        return result.to_input_list()
     elif runner_mode == 'stream':
         result = Runner.run_streamed(agent, input_items)
         async for event in result.stream_events():
             if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
                 print(event.data.delta, end="", flush=True)
-        return result.to_input_list()
+    return result
 
 
 async def agents_chat_continuous(agent: Agent, runner_mode: Literal['async', 'stream'] = 'async'):
-    result_input_list = []
+    input_item = []
     while True:
         user_input = input("\n💬 请输入你的消息(输入quit退出):")
         if user_input.lower() in ['exit', 'quit']:
             print("✅ 对话已结束")
             break
-        result_input_list.append({"role": "user", "content": user_input})
-        result_input_list = await agents_async_chat_once(agent=agent, input_items=result_input_list, runner_mode=runner_mode)
-        result_input_list.append({"role": "user", "content": user_input})
-
+        input_item.append({"role": "user", "content": user_input})
+        result = await agents_async_chat_once(agent=agent, input_items=input_item, runner_mode=runner_mode)
+        input_item = result.to_input_list()
 
 
 if __name__ == '__main__':
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     # 运行主协程
-    asyncio.run(agents_chat_continuous(agent,  runner_mode='stream'), debug=False)
+    asyncio.run(agents_chat_continuous(agent, runner_mode='stream'), debug=False)
     # prompt = "hi"
     # result_items = asyncio.run(agents_async_chat_once(agent, prompt=prompt, runner_mode='async'), debug=False)
     # print(type(result_items))
