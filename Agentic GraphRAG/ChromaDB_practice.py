4cfea601bde7c49d1ab9c70c5093f095949d61cd
D.T <dancingonmoon@msn.com>
2026-01-05 17:10:10 +0800

    chromaDB practice

---
+++ Agentic GraphRAG/ChromaDB_practice.py
@@ -0,0 +1,14 @@
+import os
+import chromadb
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+
+# embeddingfuction:
+Qwen_embedding = OpenAIEmbeddingFunction(api_key=os.environ["DASHSCOPE_API_KEY"],
+                                         model_name="text-embedding-v4",
+                                         api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
+                                         )
+
+# Create a ChromaDB client
+chromadb_path = r"./test.db"
+test_client = chromadb.PersistentClient(path=chromadb_path)
+collection = test_client.get_or_create_collection(name="test1")
